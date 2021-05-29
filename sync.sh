5593544d5c92893603e13d19fd444de3675fb668
bbudhathoki <dragnet.sh@gmail.com>
2021-05-29 10:13:56 +0545

    copy all the dot files

---
+++ sync.sh
@@ -0,0 +1,7 @@
+cp ~/.config/alacritty/alacritty.yml ./
+cp ~/.config/nvim/init.vim ./
+cp ~/.config/karabiner/karabiner.json ./
+cp ~/.config/skhd/skhdrc ./
+cp ~/.tmux.conf ./tmux.conf
+cp ~/.config/yabai/yabairc ./
+cp ~/.zshrc ./zshrc
