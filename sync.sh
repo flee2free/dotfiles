3e7bc9cf2dddbdf7247ef17bc81902bfdcb0e24e
bbudhathoki <dragnet.sh@gmail.com>
2021-06-17 08:44:47 +0545

    Add Kitty Theme

--- sync.sh
+++ sync.sh
@@ -5,7 +5,12 @@ cp ~/.config/skhd/skhdrc ./
 cp ~/.tmux.conf ./tmux.conf
 cp ~/.config/yabai/yabairc ./
 cp ~/.zshrc ./zshrc
+
 cp ~/.config/kitty/kitty.conf ./
+cp ~/.config/kitty/kitty-themes/gruvbox_dark.conf ./kitty-theme/gruvbox_dark.conf
+cp ~/.config/kitty/kitty-themes/gruvbox_light.conf ./kitty-theme/gruvbox_light.conf
+
 cp ~/.gvimrc ./gvimrc
 cp ~/Library/Application\ Support/org.yanex.marta/conf.marco ./marta_config.marco
 cp ~/bin/toggle-theme.sh ./
+
