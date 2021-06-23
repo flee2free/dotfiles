99c529d100684c3db6a81f8d900993930faefd4c
bbudhathoki <dragnet.sh@gmail.com>
2021-06-23 16:22:16 +0545

    Autosuggest Hilight Color

--- toggle-theme.sh
+++ toggle-theme.sh
@@ -36,6 +36,7 @@ _light() {
     rm -rf ~/.config/kitty/theme.conf && ln -s ~/.config/kitty/kitty-themes/gruvbox_light.conf ~/.config/kitty/theme.conf
     sed -i "" -e "s/^\([[:space:]]*\)\(export FZF_DEFAULT_OPTS=\)\(.*\)/\1\2\$fzf_theme_light/g" ~/.zshrc
     sed -i "" -e "s/^\(export BAT_THEME=\).*/\1\"gruvbox-light\"/g" ~/.zshrc
+    sed -i "" -e "s/^\(export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=\).*/\1\"fg=180\"/g" ~/.zshrc
     sed -i "" -e "s/^\(set background=\).*/\1light/g" ~/.config/nvim/init.vim
     sed -i "" -e "s/\(.*active_window_border_color.*\)\".*\"/\1\"0xE03e5ea3\"/g" ~/.config/yabai/yabairc
 }
@@ -46,6 +47,7 @@ _dark() {
     rm -rf ~/.config/kitty/theme.conf && ln -s ~/.config/kitty/kitty-themes/gruvbox_dark.conf ~/.config/kitty/theme.conf
     sed -i "" -e "s/^\([[:space:]]*\)\(export FZF_DEFAULT_OPTS=\)\(.*\)/\1\2\$fzf_theme_dark/g" ~/.zshrc
     sed -i "" -e "s/^\(export BAT_THEME=\).*/\1\"gruvbox-dark\"/g" ~/.zshrc
+    sed -i "" -e "s/^\(export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=\).*/\1\"fg=015\"/g" ~/.zshrc
     sed -i "" -e "s/^\(set background=\).*/\1dark/g" ~/.config/nvim/init.vim
     sed -i "" -e "s/\(.*active_window_border_color.*\)\".*\"/\1\"0xE04caf50\"/g" ~/.config/yabai/yabairc
 }
